019d308753ac1152e942c50952d5fa7c81a65a7d
maineimissamu <sam.chadwick.p@gmail.com>
2025-02-08 11:06:52 +0100

    First Day
    Co-authored-by: Vikhalcito <Vikhalcito@users.noreply.github.com>

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,42 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+     constructor (questions, timeLimit, timeRemaining) {
+        
+     this.questions = questions;
+     this.timeLimit = timeLimit;
+     this.timeRemaining = timeRemaining;
+     this.correctAnswers = 0;
+     this.currentQuestionIndex = 0;
+     }
 
-    // 2. getQuestion()
+     getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+     }
     
-    // 3. moveToNextQuestion()
+     moveToNextQuestion(){
+        this.currentQuestionIndex += 1;
+     }
 
-    // 4. shuffleQuestions()
+     shuffleQuestions() {
+        for(let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [ this.questions[i],this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
 
-    // 5. checkAnswer(answer)
+     checkAnswer(answer){
+        if(this.getQuestion().answer === answer) {
+            this.correctAnswers++;
+        }
+     }
 
-    // 6. hasEnded()
+     hasEnded(){
+        if(this.currentQuestionIndex < this.questions.length){
+            return false
+        } else {
+            return true
+        }
+       
+     }
 }
